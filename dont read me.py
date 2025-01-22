c1fd5692d448aa100cd81a25798212ad7d78dd79
Mmmnzsbl <147035471+Mmmnzsbl@users.noreply.github.com>
2025-01-22 22:05:05 +0200

    Update dont read me.py

--- dont read me.py
+++ dont read me.py
@@ -1,7 +1,16 @@
+def welkom_game:
+
 print("welkom")
 print("this is a practice code for my \n"
-"all projects")
+"projects")
 print("so while we here. let me practice")
 
 while True:
+    q1 = input("hello there. guess my name. ")
+        if q1 == "mmmnzsbl":
+        print("yes. let's continue")
+        break
+    else:
+        print("nope. do your homework...")
+        return
     
